b08e9c48c5136fabf495b2d84ad9f8d66457eaa9
hua <hua@DESKTOP-0SEGRMV>
2019-06-04 21:21:37 +0800

    初次提交Spring Boot的模板

---
+++ src/main/java/com/example/weixin_lzh_1/WeixinLzh1Application.java
@@ -0,0 +1,13 @@
+package com.example.weixin_lzh_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeixinLzh1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeixinLzh1Application.class, args);
+	}
+
+}
